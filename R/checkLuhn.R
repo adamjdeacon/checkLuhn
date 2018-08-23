607411723da872cfa93cf3d7785a0603095f9f52
Adam Deacon <adam@deaconsworld.org.uk>
2018-08-23 17:37:25 +0100

    Added luhn, check digit is a bit screwy

--- R/checkLuhn.R
+++ R/checkLuhn.R
@@ -7,8 +7,32 @@
 #'
 
 checkLuhn <- function(number) {
+  # strip spaces
+  number <- gsub(pattern = " ", replacement = "", number)
+
+  # must have at least 2 digits
   if(nchar(number) <= 2) {
     return(FALSE)
   }
 
+  # Return FALSE if not a number
+  if (!grepl("^[[:digit:]]+$", number)) {
+    return(FALSE)
+  }
+
+  # split the string, convert it to a list, and reverse it
+  digits <- unlist(strsplit(number, ""))
+  digits <- digits[length(digits):1]
+
+  digits[seq(2, length(digits),2 )] <- as.numeric(digits[seq(2, length(digits),2 )]) * 2
+
+  # gonna do some maths, let's convert it to numbers
+  digits <- as.numeric(digits)
+
+  # a digit cannot be two digits, so any that are greater than 9, subtract 9 and make the
+  # world a better place
+  digits <- ifelse(digits > 9, digits - 9, digits)
+
+  # does the sum divide by 10?
+  sum(digits) %% 10 == 0
 }
