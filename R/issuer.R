7d2289ad42c3ad00fe0911cd12a95a305b64d7c1
Adam Deacon <adam@deaconsworld.org.uk>
2018-09-12 11:33:06 +0100

    Tidied up imports suppressed output of column guessing

--- R/issuer.R
+++ R/issuer.R
@@ -3,17 +3,19 @@
 #'
 #' issuer
 #' @export
+#' @import readr
 #' @param number PAN to look up
 #' @examples
 #' issuer('4111 1111 1111 1111')
 
 issuer <- function(number) {
-  library(readr)
+
   # BIN Ranges from https://www.bincodes.com/bin-list/
-  x <- read_csv(system.file('BIN.csv', package = 'checkLuhn'))
+  x <- read_csv(system.file('BIN.csv', package = 'checkLuhn'), col_types = cols())
 
   # strip spaces
   number <- gsub(pattern = " ", replacement = "", number)
+
 
   if(!checkLuhn(number)) {
     return(FALSE)
